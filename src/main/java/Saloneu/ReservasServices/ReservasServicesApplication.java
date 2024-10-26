ae9d62a25b335db25690d947f99155da3c9c2765
Ferney BG <67124673+FerneyBage@users.noreply.github.com>
2024-10-26 18:50:03 -0500

    git init

---
+++ src/main/java/Saloneu/ReservasServices/ReservasServicesApplication.java
@@ -0,0 +1,13 @@
+package Saloneu.ReservasServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Saloneu/ReservasServices/ReservasServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package Saloneu.ReservasServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
